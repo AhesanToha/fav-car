72597739dfede3c36252c41d3267ca3f17dbf06b
Toha92 <ahesan868@gmail.com>
2022-03-26 15:00:49 +0600

    cart button added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header-name'>
-            <h1>Your Fav Cars</h1>
+            <h1>Your Fav Phones</h1>
             <h2>Choose Your cars!</h2>
         </div>
     );
--- src/components/Phone/Phone.js
+++ src/components/Phone/Phone.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import "./Phone.css"
+import React from "react";
+import "./Phone.css";
 
-const Phone = ({phone}) => {
-    console.log(phone)
-    const {phone_name, image, price} = phone;
-    return (
-        <div className='phone'>
-            <img src={image} alt="" />
-            <h3>{phone_name}</h3>
-        </div>
-    );
+const Phone = ({ phone }) => {
+  console.log(phone);
+  const { phone_name, image, price } = phone;
+  return (
+    <div className="phone">
+      <img src={image} alt="" />
+      <div className="phone-info">
+        <h4 className="phone-name">{phone_name}</h4>
+        <h5>Price: $ {price}</h5>
+      </div>
+      <button className="btn-cart">Add To Cart</button>
+    </div>
+  );
 };
 
 export default Phone;
